7f8e45126df9b3444f829ac7bdc88aef284f0f32
haithemaraissia <haithemaraissia@gmail.com>
2016-10-14 15:44:57 -0400

    Getting to barely Work
    Change the Image to No Named for the spacing

--- KC MO Guide Complete/KC-MO Guide/Controller/TestViewController.swift
+++ KC MO Guide Complete/KC-MO Guide/Controller/TestViewController.swift
@@ -10,25 +10,56 @@ import UIKit
 
 class TestViewController: UIViewController {
 
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var AddressLabel: UILabel!
+    @IBOutlet weak var FaceBookButtonLabel: UIButton!
+    @IBOutlet weak var PhoneLabel: UILabel!
+    @IBOutlet weak var MainPhoto: UIImageView!
+    
     @IBOutlet weak var Test: UILabel!
     var testPassed: String = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        BindPlaceDetail()
+        
 
         // Do any additional setup after loading the view.
      // Test.text = testPassed
     }
 
-    @IBAction func faceBookButton(_ sender: AnyObject) {
-        UIApplication.shared.openURL(NSURL(string: "http://www.google.com") as! URL)    }
-    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func faceBookButton(_ sender: AnyObject)
+    {
+        UIApplication.shared.openURL(NSURL(string: "http://www.google.com") as! URL)
+    }
+    
+    
+    func BindPlaceDetail() -> Void
+    {
+        TitleLabel?.text = "This is Title"
+        AddressLabel?.text = "This is address"
+        PhoneLabel?.text = "(913) 406 0298"
+        FaceBookButtonLabel.setTitle("This is facebookTitle", for: UIControlState.normal)
+//        
+//        FaceBookButtonLabel.titleLabel?.text = "Test"
+        
+        
+//        MainPhoto.image  = UIImage(named: sections[indexPath.row].name)
 
+        
+        MainPhoto.image = UIImage( named: "noimage-1")
+
+    }
 
     /*
     // MARK: - Navigation
